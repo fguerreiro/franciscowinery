d558ce04efa735a8d5340fb0b9099acf40b1604b
Francisco Guerreiro <francisco.guerreiro.jr@gmail.com>
2017-11-13 22:56:15 -0500

    first commit

---
+++ FranciscoWinery/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using FranciscoDrinksSAQ.Security;
+using FranciscoWinery.Models;
+using Newtonsoft.Json;
+
+namespace FranciscoWinery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Search(string q = "")
+        {
+            var json = new WebClient().DownloadString(ApiToken.Url + "&q="+q);
+
+            var result = JsonConvert.DeserializeObject<ApiResult>(json);
+            
+            return View("List", result);
+        }
+    }
+}
---
+++ FranciscoWinery/Models/ApiResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace FranciscoWinery.Models
+{
+    public class ApiResult
+    {
+        public string TotalCount { get; set; }
+        public string Duration { get; set; }
+        public List<Drink> Results { get; set; }
+    }
+
+    public class Drink
+    {
+        public string Title { get; set; }
+        public string Uri { get; set; }
+        public string Excerpt { get; set; }
+    }
+}
