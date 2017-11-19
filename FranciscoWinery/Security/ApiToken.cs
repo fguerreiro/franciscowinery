1644813aebfaaf6abe94b668ae340668c9f76937
Francisco Guerreiro <francisco.guerreiro.jr@gmail.com>
2017-11-19 14:00:13 -0500

    updated gitignore to show security class

---
+++ FranciscoWinery/Security/ApiToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FranciscoDrinksSAQ.Security
+{
+    public static class ApiToken
+    {
+        public static string Token = "058c85fd-3c79-42a3-9236-b83d35588103";
+
+        //&q=Bi%C3%A8re%20rousse
+        //https://cloudplatform.coveo.com/rest/search?access_token=058c85fd-3c79-42a3-9236-b83d35588103&q=Bi%C3%A8re%20rousse
+        public static string Url = 
+            $"https://cloudplatform.coveo.com/rest/search?access_token={Token}";
+    }
+}
