99c2114c02009ca3d6da013ef4611cdb360a43d8
Francisco Guerreiro <francisco.guerreiro.jr@gmail.com>
2017-11-19 18:15:39 -0500

    Added nuget package FluentAssertions

--- FranciscoWineryTests/HomeTests.cs
+++ FranciscoWineryTests/HomeTests.cs
@@ -6,7 +6,6 @@ namespace FranciscoWineryTests
     [TestFixture]
     public class HomeTests
     {
-        
         // test: Given a no search, should return a NoResult
         
         // test: Given an invalid search, return proper helpful page
