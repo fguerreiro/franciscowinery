13726d0bc5cacdd7d6c1dfcf79eac212969e8686
Francisco Guerreiro <francisco.guerreiro.jr@gmail.com>
2017-11-19 18:19:22 -0500

    Added ref to main Project in Test proj

--- FranciscoWineryTests/HomeTests.cs
+++ FranciscoWineryTests/HomeTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FranciscoWinery.Controllers;
 using NUnit.Framework;
 
 namespace FranciscoWineryTests
@@ -6,11 +7,18 @@ namespace FranciscoWineryTests
     [TestFixture]
     public class HomeTests
     {
+        private HomeController _sut;
         // test: Given a no search, should return a NoResult
         
         // test: Given an invalid search, return proper helpful page
         
         // test: Given a valid search, return something relevant
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new HomeController();
+        }
         
         [Test]
         public void Index_ShouldReturnNoResult()
