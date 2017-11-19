73a76e978c74093e403e154cefac4ee69ce3324c
Francisco Guerreiro <francisco.guerreiro.jr@gmail.com>
2017-11-19 15:12:42 -0500

    added ajax support to populate partial view

--- FranciscoWinery/Controllers/HomeController.cs
+++ FranciscoWinery/Controllers/HomeController.cs
@@ -37,12 +37,28 @@ namespace FranciscoWinery.Controllers
             return View();
         }
         
-        [AcceptVerbs(HttpVerbs.Get)]
         public ActionResult Search(SearchOptions search)
         { 
             ApiResult result = Fetcher.GetApiResultAsync(search.QueryString);
             
-            return View("List", result);
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_SearchResult", result);
+            }
+            
+            return View("Index", result);
+        }
+        
+        public ActionResult DailyDeal(SearchOptions search)
+        { 
+            ApiResult result = Fetcher.GetApiResultAsync(search.QueryString);
+            
+            return PartialView("_SearchResult", result);
+        }
+
+        public ActionResult AutoComplete(string text)
+        {
+            return null;
         }
         
     }
@@ -57,7 +73,8 @@ public class SearchOptions
     
 
     public string QueryString => 
-        $"{Q}&sortField={SortField}&numberOfResults={NumberOfResults}&sortCriteria={SortCriteria}";
+        $"{Q}&sortField={SortField}&numberOfResults={NumberOfResults}";
+    //&sortCriteria={SortCriteria}";
 }
 
 public static class Helpers
