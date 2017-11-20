824163949ea366dfe3164d6b107d8510e0eea418
Francisco Guerreiro <francisco.guerreiro.jr@gmail.com>
2017-11-19 19:05:47 -0500

    Added passing unit tests

--- FranciscoWinery/Models/ApiResult.cs
+++ FranciscoWinery/Models/ApiResult.cs
@@ -26,18 +26,22 @@ namespace FranciscoWinery.Models
 
     public class SearchOptions
     {
-//        public double Price { get; set; }
-//        public AvailableStatus AvailableStatus { get; set; }
-//        public string Category { get; set; } 
-//        public string Country { get; set; } // todo: Value Object for validation      
         public string Q { get; set; } = string.Empty;
         public string SortField { get; set; } = "millesime";
         public string SortCriteria { get; set; } = "fielddescending";
         public int NumberOfResults { get; set; } = 12;
     
-
         public string QueryString => 
-            $"{Q}&sortField={SortField}&numberOfResults={NumberOfResults}&sortCriteria={SortCriteria}";
+            $"{Q}&numberOfResults={NumberOfResults}{GetSortCriteria()}";
+
+        private string GetSortCriteria()
+        {
+            if (SortField == "tpprixnum")
+            {
+                return $"&sortField={SortField}&sortCriteria={SortCriteria}";
+            }
+            return "";
+        }
     }
 
     public enum AvailableStatus
--- FranciscoWineryTests/HomeTests.cs
+++ FranciscoWineryTests/HomeTests.cs
@@ -3,7 +3,14 @@ using FranciscoWinery.Controllers;
 using FranciscoWinery.Models;
 using NUnit.Framework;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.AutoMoq.Extensions;
+using Moq;
+using System.Web;
+using System.Web.Routing;
 
 namespace FranciscoWineryTests
 {
@@ -11,19 +18,33 @@ namespace FranciscoWineryTests
     public class HomeTests
     {
         private HomeController _sut;
-        
-        // test: Given an invalid search, return proper helpful page
-        
-        // test: Given a valid search, return something relevant
+        private SearchOptions _searchOptions;
+        private Fixture _fixture;
 
         [SetUp]
         public void Setup()
         {
-            _sut = new HomeController();
+            _fixture = new Fixture();
 
+            var request = new Mock<HttpRequestBase>();
+            request.SetupGet(x => x.Headers).Returns(
+                new System.Net.WebHeaderCollection {
+                    
+                });
+            
+            var context = new Mock<HttpContextBase>();
+            context.SetupGet(x => x.Request).Returns(request.Object);
+            
+            _sut = new HomeController();
+            _sut.ControllerContext = 
+                new ControllerContext(context.Object, new RouteData(), _sut);
+            
+            _searchOptions = new SearchOptions()
+                {
+                    Q = "ale"
+                };
         }
         
-        // test: Given a no search, should return a NoResult
         [Test]
         public void Index_ShouldReturnNoResult()
         {
@@ -32,9 +53,38 @@ namespace FranciscoWineryTests
             Assert.Multiple(() =>
             {
                 Assert.IsNotNull(result);
-                Assert.IsNotNull(result.Model); // add additional checks on the Model
+                Assert.IsNotNull(result.Model);
                 Assert.That(result.Model is NoResult);
             });
+        }
+        
+        [Test]
+        public void Index_SearchADrink_ShouldReturnApiResult()
+        {
+            var result = _sut.Search(_searchOptions) as ViewResult;
+
+            Assert.That(result.Model is ApiResult);
+        }
+        
+        [Test]
+        public void Index_SearchAnAle_ShouldHaveAnAle()
+        {
+            var viewResult = _sut.Search(_searchOptions) as ViewResult;
+            var resultModel = viewResult.Model as ApiResult;
+
+            var aleUri = resultModel.Results.First().Uri;
+            
+            // todo: this test is fragile, if the route Uri changes, test will break
+            Assert.That(aleUri.Contains("biere-ale"));
+        }
+        
+        [Test]
+        public void Index_SearchADrink_ShouldReturnMoreThanOneItem()
+        {
+            var viewResult = _sut.Search(_searchOptions) as ViewResult;
+            var resultModel = viewResult.Model as ApiResult;
+            
+            Assert.That(resultModel.TotalCount > 0);
         }
     }
 }
